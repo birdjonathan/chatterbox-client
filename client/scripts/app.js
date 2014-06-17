7a6f3efb6d5655579c7e47a9ed358acd1b57a807
Jonathan Bird <jonbird81@gmail.com>
2014-06-17 14:01:28 -0500

    Messages are now displaying

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,10 +1,13 @@
 var app = {
   init: function(){
     this.server = "https://api.parse.com/1/classes/chatterbox";
-   var handle = this.handleSubmit;
-   $("#send .submit").on('submit', function(){
-    handle();
-  });
+    this.username = prompt(('What is your name?') || 'anonymous');
+    this.rooom = 'lobby';
+    app.updateMessages();
+    var handle = this.handleSubmit;
+    $("#send .submit").on('submit', function(){
+      handle();
+    });
   },
   send: function(message){
     $.ajax({
@@ -20,19 +23,29 @@ var app = {
       }
     });
   },
-  fetch: function(){
+  updateMessages: function(){
     $.ajax({
       url: this.server,
       type: "GET",
       contentType: 'application/json',
       success: function(data){
-        console.log(data);
+        app.clearMessages();
+        var messages = data.results;
+        for (var i = 0; i < messages.length; i++){
+          app.addMessage(messages[i]);
+        }
+
       },
       error: function(data){
-        console.log("not success");
+        console.log("failed retrieving messages");
       }
     });
   },
+  
+
+  
+  
+  
   clearMessages: function(){
     $("#chats").empty();
   },
@@ -74,4 +87,6 @@ var app = {
    app.send(message);
   },
 };
+
+app.init();
 
